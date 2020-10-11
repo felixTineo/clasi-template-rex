7a156d50151cedbe4cff8283e8b3ffc38abe94c5
felix tineo <ftandcompany@gmail.com>
2020-10-11 14:16:26 -0400

    final ??

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/felix/Documentos/projects/clasi-templates/rex/.cache/dev-404-page.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/home/felix/Documentos/projects/clasi-templates/rex/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/home/felix/Documentos/projects/clasi-templates/rex/src/pages/contact.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/felix/Documentos/projects/clasi-templates/rex/src/pages/index.js"))),
